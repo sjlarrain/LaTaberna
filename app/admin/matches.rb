d40676219f2ff5174a8f3e326eabf1e122539c80
Santiago <sjlarrain@uc.cl>
2021-08-18 21:57:37 -0400

    ActiverAdmin :cop:

---
+++ app/admin/matches.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Match do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :state, :set1p1, :set1p2, :set2p1, :set2p2, :set3p1, :set3p2, :id_match
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:state, :set1p1, :set1p2, :set2p1, :set2p2, :set3p1, :set3p2, :id_match]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
