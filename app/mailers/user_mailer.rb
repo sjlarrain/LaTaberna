d3495a23375be254cbb5432be85854582db374ae
Santiago <sjlarrain@uc.cl>
2021-08-19 12:00:57 -0400

    Mails :mailbox:  and routes :car:

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,41 @@
+class UserMailer < ApplicationMailer
+    default from: Mouses <'mouses@clataberna.com'>
+    @@url  = 'https://clataberna.herokuapp.com/login'
+    
+    def registration_email
+        @user = params[:user]
+        mail(
+            to: @user.email,
+            subject: "Inscripción Campeonato La Tarberna"
+        )
+    end
+    
+    def partner_email
+        @user = params[:user]
+        @partner = params[:partner]
+        mail(
+            to: @partner.email
+            subject: "Solicitud de Pareja"
+        )
+
+    end
+
+    def acceptance_email
+        @user = params[:user]
+        @partner = params[:partner]
+        mail(
+            to: @user.email
+            subject: "Solicitud aceptada"
+        )
+    end
+
+    def rejection_email
+        @user = params[:user]
+        @partner = params[:partner]
+        mail(
+            to: @user.email
+            subject: "Solicitud Rechazada"
+        )
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   resources :matches
   resources :partners
   devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  },
+                     path: '',
+                     path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
